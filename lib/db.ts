ce96a5ce4148c3a5858ab2eac41b65cf9b4f1a41
Oyeins-GUI <oyeinx7@gmail.com>
2026-02-04 17:38:09 +0100

    chore: setup better-auth and mongodb

---
+++ lib/db.ts
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+export let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+   // Prevent hot-reload issues in dev
+   const globalWithMongo = global as typeof globalThis & {
+      _mongoClientPromise?: Promise<MongoClient>;
+   };
+
+   if (!globalWithMongo._mongoClientPromise) {
+      client = new MongoClient(MONGODB_URI, {});
+      globalWithMongo._mongoClientPromise = client.connect();
+   }
+
+   clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+   client = new MongoClient(MONGODB_URI, {});
+   clientPromise = client.connect();
+}
+
+export const db = (await clientPromise).db();
